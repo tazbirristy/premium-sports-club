6a4d918468039bf70efb364a1269125c00a615b8
tazbirristy <tazbirristyweb@gmail.com>
2022-09-29 15:48:17 +0600

    responsive

--- src/components/Sport/Sport.js
+++ src/components/Sport/Sport.js
@@ -6,7 +6,7 @@ const Sport = ({sport}) => {
     return (
         <div>
            <div className='sport-container'>
-        <img src={img} alt="" />
+        <img className='w-full rounded-lg' src={img} alt="" />
         <p className='text-2xl mt-6 font-bold text-black-800'>{name}</p>
         <p className='text-1xl mt-2 font-bold text-black-800'>Time Required: {time} m</p>
         <button className='bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-14 rounded mt-4'>Add to List</button>
